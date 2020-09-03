85730651e6e6ef040560dd40a36a0536e7cfd138
Subhrajyoti Mondal <subhrajyoti.mondol@gmail.com>
2020-09-03 12:01:57 -0700

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,9 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i = - 0; i < nums.length; i++){
-            if(nums[i] + nums[i+1] == target){
-                int[] result = {i, i+1};
-                return result;
+        for(int i = 0; i < nums.length; i++ ){        
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    int[] result = {i, j};
+                    return result;
+                }
             }
         }
         return null;

86ff690de83e2847dafb1624cf4b61589fe1a0b9
Subhrajyoti Mondal <subhrajyoti.mondol@gmail.com>
2020-09-03 12:17:23 -0700

    Update ReverseInteger.py

--- ReverseInteger.py
+++ ReverseInteger.py
@@ -1,8 +1,21 @@
 class Solution(object):
     def reverse(self, x):
         rev = 0
+        negBool = None
+        
+        if (x < 0):
+            x = abs(x)
+            negBool = True
+        
         while(x > 0): 
             a = x % 10
             rev = rev * 10 + a 
             x = x // 10
+            
+        if (negBool):
+            rev = rev * -1
+            
+        if (rev > 2147483647 or rev < -2147483647):
+            rev = 0
+        
         return rev

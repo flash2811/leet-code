f40d7e11cd267fddcb24d1ad6d05bf7698352125
Subhrajyoti Mondal <subhrajyoti.mondol@gmail.com>
2020-09-03 12:48:06 -0700

    Update ReverseInteger.java

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,11 +1,28 @@
 class Solution {
     public int reverse(int x) {
-        int rev = 0; 
+        long rev = 0; 
+        boolean negBool = false;
+              
+         if (x < 0){
+            x = x * -1;
+            negBool = true;          
+         }
+        
         while(x > 0) 
         { 
-            rev = rev * 10 + x % 10; 
-            x = x / 10; 
+            int a = x % 10;
+            rev = rev * 10 + a; 
+            x = x / 10;
         } 
-        return rev; 
+        
+        if (negBool){
+            rev = rev * -1;     
+        }
+            
+        if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE){
+            rev = 0;            
+        }
+        
+        return (int)rev; 
     }
 }

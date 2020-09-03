ecbb0dcf7fe4629f4328aead2f4bd6f786012ec9
Subhrajyoti Mondal <subhrajyoti.mondol@gmail.com>
2020-09-03 11:19:00 -0700

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,10 +1,5 @@
 class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+    def twoSum(self, nums, target)
         for n in range(len(nums)):
             if nums[n] + nums[n+1] == target:
                 return [n, n+1]

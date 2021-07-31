6132a7cf6f055af244c1a19a73c6f870171275df
Subhra Mondal <subhrajyoti.mondol@gmail.com>
2021-07-30 18:55:13 -0700

    Create MergekSortedLists.java

---
+++ MergekSortedLists.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        List<Integer> l = new ArrayList<Integer>();
+        
+        for(ListNode ln : lists) {
+            while (ln != null) {
+                l.add(ln.val);
+                ln = ln.next;
+            }
+        }
+        
+        Collections.sort(l);
+ 
+        ListNode head = new ListNode(0);
+        ListNode h = head;
+
+        for (int i : l) {
+            ListNode t = new ListNode(i);
+            h.next = t;
+            h = h.next;
+        }
+        
+        h.next = null;
+        
+        return head.next;
+    }
+}

01b5e9311be0ddc3d283036c3ba57421af1485c7
Subhra Mondal <subhrajyoti.mondol@gmail.com>
2021-08-05 17:27:27 -0700

    Update PalindromeLinkedList.java

--- PalindromeLinkedList.java
+++ PalindromeLinkedList.java
@@ -39,4 +39,29 @@ class Solution {
         }
         return prev;
     }
+    
+    //with arrayList
+     public boolean isPalindrome(ListNode head) {
+        List<Integer> vals = new ArrayList<>();
+
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            vals.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+
+        int front = 0;
+        int back = vals.size() - 1;
+        while (front < back) {
+            if (!vals.get(front).equals(vals.get(back))) {
+                return false;
+            }
+
+            //if not the same, then check the next front and back
+            front++;
+            back--;
+        
+        }
+        return true;
+    }
 }
